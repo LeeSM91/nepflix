364ad078bffd6c0acf9f1b514411c6e90099509a
innovatoraakash <ghimiresky2@gmail.com>
2021-02-19 21:21:22 +0545

    deploy4

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 
-let socket = io.connect("//projectnepflix.herokuapp.com:4000/");
+let socket = io.connect("//projectnepflix.herokuapp.com:4000");
 let divVideoChatLobby = document.getElementById("video-chat-lobby");
 let divVideoChat = document.getElementById("video-chat-room");
 let joinButton = document.getElementById("join");
